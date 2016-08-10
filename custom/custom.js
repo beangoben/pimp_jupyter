cf1eeed7374caf119bf8988f96922902c80e17b0
benjamin sanchez <beangoben@gmail.com>
2016-08-10 09:38:03 -0400

    Update custom.js

--- custom/custom.js
+++ custom/custom.js
@@ -1,16 +1,12 @@
-
-
-if (Jupyter) {
-  $(Jupyter.events).on("app_initialized.NotebookApp", function() {
-
-    var utils = require("base/js/utils");
-    IPython.Cell.options_default.cm_config.lineNumbers = true;
-
-  });
-} else {
-  $([IPython.events]).on("app_initialized.NotebookApp", function() {
-    IPython.Cell.options_default.cm_config.lineNumbers = true;
-  });
-}
-
-IPython.Cell.options_default.cm_config.lineNumbers = true;
+define([
+    'base/js/namespace',
+    'base/js/events'
+    ],
+    function(IPython, events) {
+        events.on("app_initialized.NotebookApp",
+            function () {
+                IPython.Cell.options_default.cm_config.lineNumbers = true;
+            }
+        );
+    }
+);
